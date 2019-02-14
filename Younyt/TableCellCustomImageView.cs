12d2f9f3f8ab5f0bd6d57b086fbcf09f86e72d80
Nandha <nandhakumar@tillidsoft.com>
2019-02-14 19:27:08 +0530

    changes UI

--- Younyt/TableCellCustomImageView.cs
+++ Younyt/TableCellCustomImageView.cs
@@ -35,13 +35,27 @@ namespace Younyt
 
             // the offset here is 40 you can play with it to increase / decrease the curve height
 
-            var path = UIBezierPath.FromArc(new CGPoint(Frame.Size.Width - 10, Frame.Size.Height - 10), Frame.Size.Height / 2,
-                      180, 315, true);
+            var path = UIBezierPath.FromArc(new CGPoint(Frame.Size.Width - 5, Frame.Size.Height - 5), Frame.Size.Height / 1.5f,
+                      180, 270, true);
 
             var mask = new CAShapeLayer();
             mask.Path = path.CGPath;
             Layer.Mask = mask;
 
+            //var maskPath = UIBezierPath.FromRoundedRect(Bounds, UIRectCorner.BottomLeft | UIRectCorner.TopRight, new CGSize(Frame.Width / 1.5, Frame.Height / 1.5));
+            //var maskLayer = new CAShapeLayer();
+            //maskLayer.Frame = Bounds;
+            //maskLayer.Path = maskPath.CGPath;
+            //Layer.Mask = maskLayer;
+
+            //var frame = Frame;
+            //var remainder = CGRect.Empty;
+            //var slice = CGRect.Empty;
+
+            //frame.Divide(Frame.Height / 2, CGRectEdge.MaxYEdge, out slice, out remainder);
+            //var remainder2 = CGRect.Empty;
+            //var slice2 = CGRect.Empty;
+            //remainder.Divide(remainder.Width / 2, CGRectEdge.MaxXEdge, out slice2, out remainder2);
         }
     }
 }
